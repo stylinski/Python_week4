2b2f4804d9b605d1020bb0f7225958db97c0b5ca
stylinski <.com>
2025-08-19 12:52:53 +0300

    Week 4 Assignment

---
+++ File.py
@@ -0,0 +1,27 @@
+#creating a new file
+file = open("newfile.pdf", "w")
+file.write("Hello, this is week 4 assignment.")
+
+#Reading the file
+file = open("newfile.pdf", "r")
+data = file.readline()
+print(data)
+
+#writing a modified version  to a new file
+file = open("output.txt", "w")
+file.write(data.upper())
+
+#Ask the user for a filename
+filename = input("Please enter a filename: ")
+
+#handle errors if it doesn’t exist or can’t be read.
+try:
+    file = open(filename, "r")
+    data = file.read()
+    print("File content:\n", data)
+except FileNotFoundError:
+    print("File not found.")
+except Exception as e:
+    print("Error:", e)
+finally:
+    print("File Operation Complete.")
